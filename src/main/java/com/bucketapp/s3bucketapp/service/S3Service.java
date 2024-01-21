927dcf1560aea225fe919c22e8b6b29775da46eb
Arturo <art2dit2@hotmail.com>
2024-01-21 15:21:52 +0100

    [WIP] Adding Service interface and implementation to work with S3 SDK

--- src/main/java/com/bucketapp/s3bucketapp/service/S3Service.java
+++ src/main/java/com/bucketapp/s3bucketapp/service/S3Service.java
@@ -3,9 +3,20 @@ package com.bucketapp.s3bucketapp.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface S3Service {
 
-    public String uploadFile(MultipartFile multipartFile) throws IOException;
+    String uploadFile(MultipartFile multipartFile) throws IOException;
+
+    String downloadFile(String fileName) throws IOException;
+
+    List<String> objectsInsideBuckets();
+
+    void deleteBucketWithObjectsInsideNonVersion(String bucketName);
+
+    void deleteBucketWithControlVersion(String bucketName);
+
+    void deleteEmptyBucket(String bucketName);
 
 }
--- src/main/java/com/bucketapp/s3bucketapp/service/imp/S3ServiceImpl.java
+++ src/main/java/com/bucketapp/s3bucketapp/service/imp/S3ServiceImpl.java
@@ -4,18 +4,24 @@ import com.bucketapp.s3bucketapp.service.S3Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class S3ServiceImpl implements S3Service {
     private final S3Client s3Client;
     @Value("${aws.bucket.name}")
     private String bucketName;
+
+    @Value("${upload.s3.localPath}")
+    private String localPath;
+
 
     public S3ServiceImpl(S3Client s3Client) {
         this.s3Client = s3Client;
@@ -31,6 +37,78 @@ public class S3ServiceImpl implements S3Service {
         } catch (IOException e) {
             throw new IOException(e.getMessage());
         }
+    }
+
+    @Override
+    public String downloadFile(String fileName) throws IOException {
+
+        if (!doesObjectExist(fileName)) {
+            return "This filename does not exist";
+        }
+
+        GetObjectRequest objectRequest = GetObjectRequest.builder().bucket(bucketName)
+                .key(fileName)
+                .build();
+        ResponseInputStream<GetObjectResponse> result = s3Client.getObject(objectRequest);
+        try {
+            FileOutputStream fos = new FileOutputStream(localPath + fileName);
+            byte[] readBuff = new byte[1024];
+            int read_len = 0;
+            while ((read_len = result.read(readBuff)) > 0) {
+                fos.write(readBuff, 0, read_len);
+            }
+            fos.close();
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+        return "File downloaded success";
+    }
+
+    @Override
+    public List<String> objectsInsideBuckets() {
+        return s3Client.listBuckets().buckets().stream().map(Bucket::name).toList();
+    }
+
+    @Override
+    public void deleteBucketWithObjectsInsideNonVersion(String bucketName) {
+        List<S3Object> s3Objects = s3Client.listObjects(ListObjectsRequest.builder().bucket(bucketName).build()).contents();
+        if (!s3Objects.isEmpty()) {
+            for(S3Object s3Object: s3Objects){
+                s3Client.deleteObject(DeleteObjectRequest.builder().bucket(bucketName).key(s3Object.key()).build());
+                s3Client.deleteBucket(DeleteBucketRequest.builder().bucket(bucketName).build());
+            }
+
+        }
+    }
+
+    @Override
+    public void deleteBucketWithControlVersion(String bucketName) {
+
+    }
+
+    @Override
+    public void deleteEmptyBucket(String bucketName) {
+        s3Client.deleteBucket(DeleteBucketRequest.builder()
+                .bucket(bucketName)
+                .build());
+    }
+
+    private boolean doesObjectExist(String objectkey) {
+        //TODO improve this code
+        try {
+            HeadObjectRequest headObjectRequest = HeadObjectRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .key(objectkey)
+                    .build();
+            s3Client.headObject(headObjectRequest);
+            return true;
+        } catch (S3Exception exception) {
+            if (exception.statusCode() == 404) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private PutObjectRequest buildObjectRequest(String fileName) {
