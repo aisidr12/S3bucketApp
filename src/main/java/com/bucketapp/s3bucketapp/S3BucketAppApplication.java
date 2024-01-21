b6decfeec410b1953a4b4af1f2aaa7aa0744c15c
Arturo <art2dit2@hotmail.com>
2024-01-21 01:00:31 +0100

    first commit

---
+++ src/main/java/com/bucketapp/s3bucketapp/S3BucketAppApplication.java
@@ -0,0 +1,13 @@
+package com.bucketapp.s3bucketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BucketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3BucketAppApplication.class, args);
+    }
+
+}
