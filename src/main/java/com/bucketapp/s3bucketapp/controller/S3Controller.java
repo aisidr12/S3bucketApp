f4a0251b9e2200ba136976631ba81f35cdeebfd0
Arturo <art2dit2@hotmail.com>
2024-01-21 11:11:17 +0100

    [WIP] Uploading file from controller

--- src/main/java/com/bucketapp/s3bucketapp/controller/S3Controller.java
+++ src/main/java/com/bucketapp/s3bucketapp/controller/S3Controller.java
@@ -1,7 +1,24 @@
 package com.bucketapp.s3bucketapp.controller;
 
+import com.bucketapp.s3bucketapp.service.S3Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class S3Controller {
+
+    private final S3Service s3Service;
+
+    public S3Controller(S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        return s3Service.uploadFile(file);
+    }
 }
